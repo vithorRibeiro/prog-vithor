b4ea6ef1a7a26a962529c4f8e6c071819ad6dfbb
vithorRibeiro <127416872+vithorRibeiro@users.noreply.github.com>
2024-08-12 08:01:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,13 +30,21 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0) {
         return `${dias} dias ${horas} horas ${minutos} minutos ${segundos} segundos`;
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 }
 
 function atualizaCronometro() {
+    document.getElementById("dias0").textContent =
+calculaTempo(tempos[0])[0];
+document.getElementById("horas0").textContent =
+calculaTempo(tempos[0])[1];
+document.getElementById("min0").textContent =
+calculaTempo(tempos[0])[2];
+document.getElementById("seg0").textContent =
+calculaTempo(tempos[0])[3];
     contadores.forEach((contador, i) => {
-        contador.textContent = calculaTempo(tempos[i]);
+        //contador.textContent = calculaTempo(tempos[i]);
     });
 }
 
@@ -44,4 +52,7 @@ function comecaCronometro() {
     atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
 }
-comecaCronometro();
+//comecaCronometro();
+
+
+
